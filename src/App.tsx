4057ad81f7cd14be466d36f2d747fc1c8d7f4ad8
제리 <jaryapp@gmail.com>
2020-11-22 16:21:41 +0900

    env: set init enviroment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* External dependencies */
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component={}/>
+    </Switch>
+  );
+}
+
+export default App;
