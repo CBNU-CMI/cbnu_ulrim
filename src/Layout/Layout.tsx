113e4afd767dcf13422ccd8a5ad4efec8dba781f
nohgijin <ynda11111@naver.com>
2020-11-22 21:38:03 +0900

    [#3] navigation event 부여 어느정도 스크롤시 navigation의 스타일을 변경

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,16 +1,40 @@
 /* External dependencies */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 
 /* Internal dependencies */
 import styles from './Layout.module.scss';
 import Navigation from 'components/Global/Navigation';
+
 const cx = classNames.bind(styles);
 
 function Layout({ children }) {
+  const [downState, setDownState] = useState(false);
+  const callback = entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setDownState(false);
+      } else {
+        setDownState(true);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, {
+    rootMargin: '100px 100px',
+  });
+
+  useEffect(() => {
+    observer.observe(document.querySelector<any>('.fake'));
+  }, []);
+
   return (
     <div className={cx('main-layout')}>
-      <Navigation />
+      <div className="fake" />
+      {downState ? (
+        <Navigation className="down" />
+      ) : (
+        <Navigation className="" />
+      )}
       {children}
     </div>
   );
