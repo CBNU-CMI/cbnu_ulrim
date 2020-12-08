4f5a7cd73dcf6aaec4ae8ece2ab0da2fe3362893
nohgijin <ynda11111@naver.com>
2020-12-08 15:56:10 +0900

    [#28] feat: scroll event로 무한스크롤 구현

--- src/components/PetitionList/PetitionList.tsx
+++ src/components/PetitionList/PetitionList.tsx
@@ -1,5 +1,5 @@
 /* External dependencies */
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import classNames from 'classnames/bind';
 
 /* Internal dependencies */
@@ -10,6 +10,33 @@ import mockData from './mockData.json';
 const cx = classNames.bind(styles);
 
 function PetitionList() {
+  const [itemIndex, setItemIndex] = useState(0);
+  const [contents, setContents] = useState(mockData.slice(0, 5));
+
+  const _infiniteScroll = useCallback(() => {
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight,
+    );
+    let scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop,
+    );
+    let clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight === scrollHeight) {
+      setItemIndex(itemIndex + 5);
+      setContents(
+        contents.concat(mockData.slice(itemIndex + 5, itemIndex + 10)),
+      );
+    }
+  }, [itemIndex, contents]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', _infiniteScroll, true);
+    return () => window.removeEventListener('scroll', _infiniteScroll, true);
+  }, [_infiniteScroll]);
+
   return (
     <div className={cx('petition_list')}>
       <div className={cx('petition_new')}>최신 청원들</div>
@@ -23,7 +50,7 @@ function PetitionList() {
           <span className={cx('end')}>청원마감일</span>
           <span className={cx('agree-num')}>참여인원</span>
         </div>
-        {mockData.map(petition => {
+        {contents.map(petition => {
           return <PetitionCard key={petition.id} petition={petition} />;
         })}
       </div>
